6d33ff9d468d2bd4d940f812e9fdff23ec18fad0
Ammar <ammarfiveseven@gmail.com>
2025-10-06 16:06:49 -0700

    added cmp source toggle

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -79,26 +79,101 @@ return {
 
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+    local cmp = require("cmp")
+
+    -- Base list of all possible sources
+    local base_sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "path" },
+    }
+
+    -- Track disabled sources per buffer
+    local disabled_by_buf = {}
+
+    -- Helper to build active sources for current buffer
+    local function compute_sources(bufnr)
+      local disabled = disabled_by_buf[bufnr] or {}
+      local out = {}
+      for _, s in ipairs(base_sources) do
+        if not disabled[s.name] then
+          table.insert(out, s)
+        end
+      end
+      return out
+    end
+
+    -- Apply updated sources to cmp in this buffer
+    local function apply_sources(bufnr)
+      bufnr = bufnr or vim.api.nvim_get_current_buf()
+      cmp.setup.buffer({ sources = compute_sources(bufnr) })
+    end
+
+    -- Define :CmpToggleSource command
+    vim.api.nvim_create_user_command("CmpToggleSource", function(opts)
+      local bufnr = vim.api.nvim_get_current_buf()
+      disabled_by_buf[bufnr] = disabled_by_buf[bufnr] or {}
+
+      local name = opts.args
+      local cur = disabled_by_buf[bufnr][name]
+
+      if cur then
+        disabled_by_buf[bufnr][name] = nil
+        vim.notify("cmp: enabled source '" .. name .. "' for this buffer")
+      else
+        disabled_by_buf[bufnr][name] = true
+        vim.notify("cmp: disabled source '" .. name .. "' for this buffer")
+      end
+
+      apply_sources(bufnr)
+    end, {
+        nargs = 1,
+        complete = function()
+          return vim.tbl_map(function(s) return s.name end, base_sources)
+        end,
+      })
+
+    -- Default state: disable nvim_lsp globally
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function(args)
+        local bufnr = args.buf
+        disabled_by_buf[bufnr] = disabled_by_buf[bufnr] or {}
+        disabled_by_buf[bufnr]["nvim_lsp"] = true -- disable by default
+        apply_sources(bufnr)
+      end,
+    })
+
+    -- CMP setup
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          require("luasnip").lsp_expand(args.body)
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
-      sources = cmp.config.sources({
-        { name = 'luasnip' }, -- For luasnip users.
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-        { name = 'path' },
-      }
-      )
+      sources = {
+        { name = "luasnip" }, -- enabled by default
+        -- nvim_lsp intentionally not listed here (disabled by default)
+      },
     })
+
+    -- Optional: quick keybind to toggle interactively
+    vim.keymap.set("n", "<leader>ct", function()
+      vim.cmd("CmpToggleSource " .. vim.fn.input("cmp source: "))
+    end, { desc = "Toggle nvim-cmp source in current buffer" })
+
+    -- vim.g.cmp_enabled = true
+    -- cmp.setup({ enabled = function() return vim.g.cmp_enabled end })
+    -- vim.api.nvim_create_user_command('CmpToggle', function()
+    --   vim.g.cmp_enabled = not vim.g.cmp_enabled
+    --   vim.notify('cmp: ' .. (vim.g.cmp_enabled and 'enabled' or 'disabled'))
+    -- end, {})
 
     vim.diagnostic.config({
       -- update_in_insert = true,
